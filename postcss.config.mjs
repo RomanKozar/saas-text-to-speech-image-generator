cd0d9e68095a8c263251e6459a3aaa09dc74d186
RomanKozar <kozarrma2004@gmail.com>
2025-04-29 23:08:18 +0300

    setup project

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,5 +1,21 @@
-const config = {
-  plugins: ["@tailwindcss/postcss"],
-};
+// module.exports = {
+// 	plugins: {
+// 		'@tailwindcss/postcss': {},
+// 		autoprefixer: {},
+// 	},
+// }
 
-export default config;
+// module.exports = {
+// 	plugins: {
+// 		tailwindcss: {},
+// 		autoprefixer: {},
+// 	},
+// }
+/** @type {import('postcss-load-config').Config} */
+const config = {
+	plugins: {
+		tailwindcss: {},
+	},
+}
+
+export default config
