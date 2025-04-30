9fdfe2c9ced437a565dac4dcd617e352cb467d24
RomanKozar <kozarrma2004@gmail.com>
2025-04-30 20:03:57 +0300

    setup is done

---
+++ postcss.config.js
@@ -0,0 +1,21 @@
+// module.exports = {
+// 	plugins: {
+// 		'@tailwindcss/postcss': {},
+// 		autoprefixer: {},
+// 	},
+// }
+
+module.exports = {
+	plugins: {
+		tailwindcss: {},
+		autoprefixer: {},
+	},
+}
+// /** @type {import('postcss-load-config').Config} */
+// const config = {
+// 	plugins: {
+// 		tailwindcss: {},
+// 	},
+// }
+
+// export default config
