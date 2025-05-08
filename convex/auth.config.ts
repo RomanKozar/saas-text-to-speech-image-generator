ce4045bc607a45aa6c4311803001bc7b81e9f307
RomanKozar <kozarrma2004@gmail.com>
2025-05-08 13:06:11 +0300

    connecting clerk and added sign-in and sign-up table creation in convex

---
+++ convex/auth.config.ts
@@ -0,0 +1,12 @@
+//https://sensible-insect-91.clerk.accounts.dev
+
+const authConfig = {
+	providers: [
+		{
+			domain: 'https://sensible-insect-91.clerk.accounts.dev/',
+			applicationID: 'convex',
+		},
+	],
+}
+
+export default authConfig
