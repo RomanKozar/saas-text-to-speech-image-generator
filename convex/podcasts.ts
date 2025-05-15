d439dfad9ca1114280e80139c1fe73c95550c9bc
RomanKozar <kozarrma2004@gmail.com>
2025-05-15 16:56:01 +0300

    add datail page

--- convex/podcasts.ts
+++ convex/podcasts.ts
@@ -1,6 +1,61 @@
+import { channel } from 'diagnostics_channel'
+
 import { ConvexError, v } from 'convex/values'
+
 import { mutation, query } from './_generated/server'
 
+// створити мутацію подкасту
+export const createPodcast = mutation({
+	args: {
+		audioStorageId: v.id('_storage'),
+		title: v.string(),
+		description: v.string(),
+		audioUrl: v.string(),
+		imageUrl: v.string(),
+		imageStorageId: v.id('_storage'),
+		voicePrompt: v.string(),
+		imagePrompt: v.string(),
+		voiceType: v.string(),
+		views: v.number(),
+		audioDuration: v.number(),
+	},
+	handler: async (ctx, args) => {
+		const identity = await ctx.auth.getUserIdentity()
+
+		if (!identity) {
+			throw new ConvexError('User not authenticated')
+		}
+
+		const user = await ctx.db
+			.query('users')
+			.filter(q => q.eq(q.field('email'), identity.email))
+			.collect()
+
+		if (user.length === 0) {
+			throw new ConvexError('User not found')
+		}
+
+		return await ctx.db.insert('podcasts', {
+			audioStorageId: args.audioStorageId,
+			userId: user[0]._id,
+			title: args.title,
+			description: args.description,
+			audioUrl: args.audioUrl,
+			imageUrl: args.imageUrl,
+			imageStorageId: args.imageStorageId,
+			// author: user[0].name,
+			// authorId: user[0].clerkId,
+			// authorImageUrl: user[0].imageUrl,
+			voicePrompt: args.voicePrompt,
+			imagePrompt: args.imagePrompt,
+			voiceType: args.voiceType,
+			views: args.views,
+			audioDuration: args.audioDuration,
+		})
+	},
+})
+
+// ця мутація необхідна для генерації url після завантаження файлу до сховища
 export const getUrl = mutation({
 	args: {
 		storageId: v.id('_storage'),
@@ -10,51 +65,140 @@ export const getUrl = mutation({
 	},
 })
 
-export const createPodcast = mutation({
+// цей запит отримає всі подкасти на основі voiceType подкасту, який ми показуємо в розділі «Схожі подкасти»
+export const getPodcastByVoiceType = query({
 	args: {
-		podcastTitle: v.string(),
-		podcastDescription: v.string(),
-		audioUrl: v.string(),
-		imageUrl: v.string(),
-		voiceType: v.string(),
-		imagePrompt: v.string(),
-		voicePrompt: v.string(),
-		views: v.number(),
-		audioDuration: v.number(),
-		audioStorageId: v.id('_storage'),
-		imageStorageId: v.id('_storage'),
+		podcastId: v.id('podcasts'),
 	},
 	handler: async (ctx, args) => {
-		const indentity = await ctx.auth.getUserIdentity()
+		const podcast = await ctx.db.get(args.podcastId)
 
-		if (!indentity) {
-			throw new ConvexError('Not authenticated')
-		}
-
-		const user = await ctx.db
-			.query('users')
-			.filter(q => q.eq(q.field('email'), indentity.email))
+		return await ctx.db
+			.query('podcasts')
+			.filter(q =>
+				q.and(
+					q.eq(q.field('voiceType'), podcast?.voiceType),
+					q.neq(q.field('_id'), args.podcastId)
+				)
+			)
 			.collect()
-
-		if (user.length === 0) {
-			throw new ConvexError('User not found')
-		}
-
-		const podcast = await ctx.db.insert('podcasts', {
-			...args,
-			user: user[0]._id,
-			author: user[0].name,
-			authorId: user[0].clerkId,
-			authorImageUrl: user[0].imageUrl,
-		})
-		return podcast
 	},
 })
 
+// цей запит отримає всі подкасти
+export const getAllPodcasts = query({
+	handler: async ctx => {
+		return await ctx.db.query('podcasts').order('desc').collect()
+	},
+})
+
+// цей запит отримає подкаст за ідентифікатором podcastId
+export const getPodcastById = query({
+	args: {
+		podcastId: v.id('podcasts'),
+	},
+	handler: async (ctx, args) => {
+		const podcast = await ctx.db.get(args.podcastId)
+
+		return podcast
+			?.withIndex('userId', q => q.eq('userId', podcast.userId))
+			?.collect()
+	},
+})
+
+// цей запит отримає подкасти на основі переглядів подкасту, які ми показуємо в розділі «Популярні подкасти»
 export const getTrendingPodcasts = query({
 	handler: async ctx => {
-		const podcasts = await ctx.db.query('podcasts').collect()
+		const podcast = await ctx.db.query('podcasts').collect()
 
-		return podcasts
+		return podcast.sort((a, b) => b.views - a.views).slice(0, 8)
+	},
+})
+
+// TODO
+export const getPodcastByAuthorId = query({
+	args: {
+		authorId: v.id('users'),
+	},
+	handler: async (ctx, args) => {
+		const user = await ctx.db.get(args.authorId)
+		// const podcasts = await ctx.db
+		//   .query("podcasts")
+		//   .filter((q) => q.eq(q.field("userId"), args.authorId))
+		//   .collect();
+		const podcasts = user?.podcastIds.map(ctx.db.get)
+
+		const totalListeners = podcasts?.reduce(
+			(sum, podcast) => sum + podcast?.views,
+			0
+		)
+
+		return { podcasts, listeners: totalListeners }
+	},
+})
+
+// за цим запитом буде отримано подкаст за пошуковим запитом
+export const getPodcastBySearch = query({
+	args: {
+		search: v.string(),
+	},
+	handler: async (ctx, args) => {
+		if (args.search === '') {
+			return await ctx.db.query('podcasts').order('desc').collect()
+		}
+
+		const titleSearch = await ctx.db
+			.query('podcasts')
+			.withSearchIndex('search_title', q => q.search('title', args.search))
+			.take(10)
+
+		if (titleSearch.length > 0) {
+			return titleSearch
+		}
+
+		return await ctx.db
+			.query('podcasts')
+			.withSearchIndex('search_body', q =>
+				q.search('description' || 'title', args.search)
+			)
+			.take(10)
+	},
+})
+
+// ця мутація оновить перегляди подкасту
+export const updatePodcastViews = mutation({
+	args: {
+		podcastId: v.id('podcasts'),
+	},
+	handler: async (ctx, args) => {
+		const podcast = await ctx.db.get(args.podcastId)
+
+		if (!podcast) {
+			throw new ConvexError('Podcast not found')
+		}
+
+		return await ctx.db.patch(args.podcastId, {
+			views: podcast.views + 1,
+		})
+	},
+})
+
+// ця мутація видалить подкаст
+export const deletePodcast = mutation({
+	args: {
+		podcastId: v.id('podcasts'),
+		imageStorageId: v.id('_storage'),
+		audioStorageId: v.id('_storage'),
+	},
+	handler: async (ctx, args) => {
+		const podcast = await ctx.db.get(args.podcastId)
+
+		if (!podcast) {
+			throw new ConvexError('Podcast not found')
+		}
+
+		await ctx.storage.delete(args.imageStorageId)
+		await ctx.storage.delete(args.audioStorageId)
+		return await ctx.db.delete(args.podcastId)
 	},
 })
